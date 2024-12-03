465553505427349684f5e1d5bbcd036e43d18bf7
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2024-12-03 12:50:14 -0500

    Updated assignments to properly show city name

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,9 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers,getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const cities = getCities()
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -18,22 +19,36 @@ const findWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+//function to find the city the walker lives in
+const findWalkerCity =(walker, cities) => {
+    let walkerCity = null
+
+    for (const city of cities) {
+        if(city.id === walker.cityId){
+            walkerCity = city
+        }
+    }
+    return walkerCity
+}
+
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
-        assignmentHTML += `
+        const currentWalkerCity = findWalkerCity(currentPetWalker,cities)
+
+            assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
-            </li>
-        `
+                ${currentPetWalker.name} in ${currentWalkerCity.name}
+            </li>`
+        }
+        assignmentHTML += "</ul>"
+
+        return assignmentHTML
+        
     }
 
-    assignmentHTML += "</ul>"
-
-    return assignmentHTML
-}
-
+    
